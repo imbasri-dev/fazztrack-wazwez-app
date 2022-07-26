014d5582d41f49132fb34a2f881ca012f7f76abb
imbasri.dev <imbasri.dev@gmail.com>
2022-07-26 22:10:22 +0700

    fix add button input. output: hasil input value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,8 @@
+// testing connect javascript console
 console.log("hello world");
 // tambah tugas start
 let buttonTambahTugas = document.querySelector("#tambah_tugas");
 // tambah tugas end
-// subtask dropdown start
-let subTaskBar = document.querySelector("#subTaskBar");
-let taskButton = document.querySelector(".task_dropdown");
-taskButton.addEventListener("click", function () {
-  // jika ada class task_drop maka akan
-  if (taskButton.classList.contains("task_dropdown")) {
-    // active , jika salah
-    taskButton.classList.add("rotate");
-    taskButton.classList.remove("task_dropdown");
-  } // active
-  else {
-    taskButton.classList.remove("rotate");
-    taskButton.classList.add("task_dropdown");
-  }
-  subTaskBar.classList.toggle("show");
-});
-// subtask dropdown end
-
-// subtask content start
-let deleteSubTask = document.querySelectorAll(".subtask_content_delete");
-let contentSubTask = document.querySelectorAll("#subtask_content");
-
-for (let i = 0; i < deleteSubTask.length; i++) {
-  deleteSubTask[i].addEventListener("click", function () {
-    let konfirm = confirm("Apakah dihapus ?");
-    if (konfirm == true) {
-      contentSubTask[i].remove(".subtask_content");
-    }
-  });
-}
-// subtask content end
-
-// modal more start
-let modalButton = document.querySelector(".more");
-let modalBar = document.querySelector(".modal_content");
-modalButton.addEventListener("click", function () {
-  if (modalBar.style.display === "block") {
-    modalBar.style.display = "none";
-  } else {
-    modalBar.style.display = "block";
-  }
-});
-// modal more end
 
 // tambah task start
 let tambahTask = document.querySelector(".todolist_right");
@@ -88,32 +46,103 @@ function handleEnter() {
     desc: document.getElementById("inputDesc").value,
     date: document.getElementById("inputDate").value,
   };
-  let newTask = document.getElementById("newTask");
-  newTask.innerHTML += `
-      <div class=" task_content_bar" id="newTaskBar ">
-      <div class="task_content" id="newTask">
-                <input type="checkbox" name="task" value="task" id="${task.name}" />
-                <label for="${task.name}"></label>
-                <span class="title_content">${task.name}</span>
-                <span class="sort_content">${task.date}</span>
-                <img src="/assets/img/more-vertical.png" alt="more" class="more">
-                <!-- task modal start -->
-                <div class="task_content_modal" id="modalTask">
-                  <div class="modal_content">
-                    <div class="modal_task edit">
-                      <img src="/assets/img/Iconly-Bold-Edit.png" alt="rename">
-                      <p>Rename Task</p>
-                    </div>
-                    <div class="modal_task delete">
-                      <img src="/assets/img/Iconly-Bold-Delete.png" alt="delete">
-                      <p>Delete Task</p>
+
+  let newTask = document.getElementById("inputBaru");
+  newTask.innerHTML += `<div class="task_content_bar" id="newTaskBar ">
+                <div class="task_content" id="newTask">
+                  <input type="checkbox" name="task" value="task" id="${task.name}" />
+                  <label for="${task.name}"></label>
+                  <span class="title_content">${task.name}</span>
+                  <span class="sort_content">${task.date}</span>
+                  <img src="/assets/img/more-vertical.png" alt="more" class="more">
+                  <!-- task modal start -->
+                  <div class="task_content_modal" id="modalTask">
+                    <div class="modal_content">
+                      <div class="modal_task edit">
+                        <img src="/assets/img/Iconly-Bold-Edit.png" alt="rename">
+                        <p>Rename Task</p>
+                      </div>
+                      <div class="modal_task delete">
+                        <img src="/assets/img/Iconly-Bold-Delete.png" alt="delete">
+                        <p>Delete Task</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <!-- task modal end -->
-                <p class="text_content">${task.desc}</p>
+                  <!-- task modal end -->
 
+                  <p class="text_content">${task.desc}</p>
+                </div>
+                <img src="/assets/img/dropdown black.png" alt="dropdown" class="task_dropdown" id="dropTaskContent" />
               </div>
+              <!-- content 1 end -->
+              <!-- subtask dropdown start -->
+              <div class="subtask_bar show" id="subTaskBar">
+                <div class="subtask_head">
+                  <h4>Subtask</h4>
+                  <button type="submit"><img src="/assets/img/Iconly-Curved-Plus.svg">Tambah</button>
+                </div>
+                <!-- subtask  start-->
+                <div class="subtask_content" id="subtask_content">
+                  <div class="subtask_input">
+                    <input type="checkbox" id="subtask1" />
+                    <label for="subtask1"></label>
+                    <span>Design Review</span>
+                  </div>
+                  <img src="/assets/img/trash-delete.png" alt="Tambah Task" class="subtask_content_delete">
+                </div>
+                <!-- subtask  end-->
               </div>
-              `;
+              <!-- subtask dropdown end -->
+              <!-- move hasil input  start-->
+              <div id="inputBaru">
+              </div>`;
+
+  // subtask dropdown start
+  let subTaskBar = document.querySelectorAll("#subTaskBar");
+  let taskButton = document.querySelectorAll(".task_dropdown");
+  // jika ada class task_drop maka akan
+  for (let index = 0; index < taskButton.length; index++) {
+    taskButton[index].addEventListener("click", function () {
+      if (taskButton[index].classList.contains("task_dropdown")) {
+        // active , jika salah
+        taskButton[index].classList.add("rotate");
+        taskButton[index].classList.remove("task_dropdown");
+      } // active
+      else {
+        taskButton[index].classList.remove("rotate");
+        taskButton[index].classList.add("task_dropdown");
+      }
+      // ini saya loop berdasarkan dari jumlah taskdropdown dikarenakan 1 paket
+      subTaskBar[index].classList.toggle("show");
+    });
+  }
+  // subtask dropdown end
+
+  // modal more start
+  let modalButton = document.querySelectorAll(".more");
+  let modalBar = document.querySelectorAll(".modal_content");
+  let windowsClick = document.getElementsByTagName("body")[0];
+  for (let modalNode = 0; modalNode < modalButton.length; modalNode++) {
+    modalButton[modalNode].addEventListener("click", function () {
+      if (modalBar[modalNode].style.display === "block") {
+        modalBar[modalNode].style.display = "none";
+      } else {
+        modalBar[modalNode].style.display = "block";
+      }
+    });
+  }
+  // modal more end
+
+  // subtask content start
+  let deleteSubTask = document.querySelectorAll(".subtask_content_delete");
+  let contentSubTask = document.querySelectorAll("#subtask_content");
+  for (let i = 0; i < deleteSubTask.length; i++) {
+    deleteSubTask[i].addEventListener("click", function () {
+      let konfirm = confirm("Apakah dihapus ?");
+      if (konfirm == true) {
+        contentSubTask[i].remove(".subtask_content");
+      }
+    });
+  }
+  // subtask content end
 }
